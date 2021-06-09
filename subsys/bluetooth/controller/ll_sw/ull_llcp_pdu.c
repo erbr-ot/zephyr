c75d2d955b46999a734ababfda09674a905ad081
Erik Brockhoff <erbr@oticon.com>
2021-06-09 09:12:17 +0200

    Bluetooth: controller: compile muc re. central/periph kconfig
    Signed-off-by: Erik Brockhoff <erbr@oticon.com>

--- subsys/bluetooth/controller/ll_sw/ull_llcp_pdu.c
+++ subsys/bluetooth/controller/ll_sw/ull_llcp_pdu.c
@@ -188,6 +188,7 @@ void ull_cp_priv_pdu_decode_feature_rsp(struct ll_conn *conn,
 /*
  * Minimum used channels Procedure Helpers
  */
+#if defined (CONFIG_BT_PERIPHERAL)
 void ull_cp_priv_pdu_encode_min_used_chans_ind(struct proc_ctx *ctx, struct pdu_data *pdu)
 {
 	struct pdu_data_llctrl_min_used_chans_ind *p;
@@ -199,12 +200,15 @@ void ull_cp_priv_pdu_encode_min_used_chans_ind(struct proc_ctx *ctx, struct pdu_
 	p->phys = ctx->data.muc.phys;
 	p->min_used_chans = ctx->data.muc.min_used_chans;
 }
+#endif /* CONFIG_BT_PERIPHERAL */
 
+#if defined (CONFIG_BT_CENTRAL)
 void ull_cp_priv_pdu_decode_min_used_chans_ind(struct ll_conn *conn, struct pdu_data *pdu)
 {
 	conn->llcp.muc.phys = pdu->llctrl.min_used_chans_ind.phys;
 	conn->llcp.muc.min_used_chans = pdu->llctrl.min_used_chans_ind.min_used_chans;
 }
+#endif /* CONFIG_BT_CENTRAL */
 #endif /* CONFIG_BT_CTLR_MIN_USED_CHAN */
 
 /*
