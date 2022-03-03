6874168d2ecc822073e43869ec645852ca7dcf74
Morten Priess <mtpr@oticon.com>
2022-03-03 17:37:08 +0100

    bluetooth: controller: Signal HCI reset to recv thread
    Synchronize HCI reset with receive thread to prevent processing of
    dangling RX nodes.
    
    Signed-off-by: Morten Priess <mtpr@oticon.com>

--- subsys/bluetooth/controller/hci/hci.c
+++ subsys/bluetooth/controller/hci/hci.c
@@ -183,6 +183,10 @@ atomic_t hci_state_mask;
 static struct k_poll_signal *hbuf_signal;
 #endif
 
+#if defined(CONFIG_BT_HCI_RESET_SIGNAL)
+static struct k_poll_signal *reset_signal;
+#endif
+
 #if defined(CONFIG_BT_CONN)
 static uint32_t conn_count;
 #endif
@@ -426,6 +430,10 @@ static void reset(struct net_buf *buf, struct net_buf **evt)
 		atomic_set_bit(&hci_state_mask, HCI_STATE_BIT_RESET);
 		k_poll_signal_raise(hbuf_signal, 0x0);
 	}
+#endif
+
+#if defined(CONFIG_BT_HCI_RESET_SIGNAL)
+	k_poll_signal_raise(reset_signal, 0x0);
 #endif
 }
 
@@ -7901,10 +7909,14 @@ uint8_t hci_get_class(struct node_rx_pdu *node_rx)
 #endif
 }
 
-void hci_init(struct k_poll_signal *signal_host_buf)
+void hci_init(struct k_poll_signal *signal_reset, struct k_poll_signal *signal_host_buf)
 {
 #if defined(CONFIG_BT_HCI_ACL_FLOW_CONTROL)
 	hbuf_signal = signal_host_buf;
+#endif
+
+#if defined(CONFIG_BT_HCI_RESET_SIGNAL)
+	reset_signal = signal_reset;
 #endif
 	reset(NULL, NULL);
 }
