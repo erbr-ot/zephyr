e914f99cb58c6e7d83ee410b1590df953db6935e
Morten Priess <mtpr@oticon.com>
2022-06-28 15:29:41 +0200

    bluetooth: controller: Fix ISO TX data pool corruption and related
    - Fixed merge error causing fragment count one too high
    - Remove unused union in struct node_tx_iso
    - Fix ISO TX data pool corruption due to multiple calls to tx_cmplt_get,
      without removing the nodes
    - Added missing release of TX node link in case of ISOAL error
    
    Signed-off-by: Morten Priess <mtpr@oticon.com>

--- subsys/bluetooth/controller/ll_sw/lll_iso_tx.h
+++ subsys/bluetooth/controller/ll_sw/lll_iso_tx.h
@@ -6,11 +6,7 @@
  */
 
 struct node_tx_iso {
-	union {
-		void        *next;
-		memq_link_t *link;
-	};
-
+	memq_link_t *link;
 	uint64_t payload_count:39; /* bisPayloadCounter/cisPayloadCounter */
 	uint64_t sdu_fragments:8;
 	uint8_t  pdu[];
