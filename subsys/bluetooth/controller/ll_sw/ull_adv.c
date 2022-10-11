14e93fbe6176cae55ff42e4f2e036c8bf8a56d5b
Troels Nilsson <trnn@demant.com>
2022-10-11 16:11:06 +0200

    Revert "Revert "Bluetooth: Controller: Fix T_MAFS between broadcasting roles""
    This reverts commit 1b62daae2703912aa7359aced12caeacfc388e73.

--- subsys/bluetooth/controller/ll_sw/ull_adv.c
+++ subsys/bluetooth/controller/ll_sw/ull_adv.c
@@ -1462,8 +1462,7 @@ uint8_t ll_adv_enable(uint8_t enable)
 				ticks_anchor + ticks_slot +
 				HAL_TICKER_US_TO_TICKS(
 					MAX(EVENT_MAFS_US,
-					    EVENT_OVERHEAD_START_US) -
-					EVENT_OVERHEAD_START_US +
+					    EVENT_OVERHEAD_START_US) +
 					(EVENT_TICKER_RES_MARGIN_US << 1));
 
 			ticks_slot_overhead_aux =
@@ -1503,8 +1502,7 @@ uint8_t ll_adv_enable(uint8_t enable)
 					ticks_anchor_aux + ticks_slot_aux +
 					HAL_TICKER_US_TO_TICKS(
 						MAX(EVENT_MAFS_US,
-						    EVENT_OVERHEAD_START_US) -
-						EVENT_OVERHEAD_START_US +
+						    EVENT_OVERHEAD_START_US) +
 						(EVENT_TICKER_RES_MARGIN_US << 1));
 
 				ret = ull_adv_sync_start(adv, sync,
