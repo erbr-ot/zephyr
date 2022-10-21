850957c3ac46095137d87c038bc911143a0ab55f
Nirosharn Amarasinghe <niag@demant.com>
2022-10-21 10:55:17 +0200

    tests: bluetooth: controller: Refactor ISO-AL unit tests to use FFF
    Switch ISO-AL TX unit tests from using mocking to using the Fake
    Function Framework.
    
    Signed-off-by: Nirosharn Amarasinghe <niag@demant.com>

--- tests/bluetooth/ctrl_isoal/src/main.c
+++ tests/bluetooth/ctrl_isoal/src/main.c
@@ -37,8 +37,8 @@ ZTEST_SUITE(test_rx_basics, NULL, NULL, isoal_test_rx_common_before, NULL, NULL)
 ZTEST_SUITE(test_rx_unframed, NULL, NULL, isoal_test_rx_common_before, NULL, NULL);
 ZTEST_SUITE(test_rx_framed, NULL, NULL, NULL, isoal_test_rx_common_before, NULL);
 
-ZTEST_SUITE(test_tx_basics, NULL, NULL, NULL, NULL, NULL);
-ZTEST_SUITE(test_tx_unframed, NULL, NULL, NULL, NULL, NULL);
-ZTEST_SUITE(test_tx_framed, NULL, NULL, NULL, NULL, NULL);
+ZTEST_SUITE(test_tx_basics, NULL, NULL, isoal_test_tx_common_before, NULL, NULL);
+ZTEST_SUITE(test_tx_unframed, NULL, NULL, isoal_test_tx_common_before, NULL, NULL);
+ZTEST_SUITE(test_tx_framed, NULL, NULL, isoal_test_tx_common_before, NULL, NULL);
 
-ZTEST_SUITE(test_tx_framed_ebq, NULL, NULL, NULL, NULL, NULL);
+ZTEST_SUITE(test_tx_framed_ebq, NULL, NULL, isoal_test_tx_common_before, NULL, NULL);
